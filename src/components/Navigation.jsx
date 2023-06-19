8aed2c15b76804f25bf8bc21d693d6c4e3878568
Alejandro Abad <alejandroabad95@gmail.com>
2023-06-19 19:55:05 +0200

    first approach

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+import './Navigation.css'
+
+//quitar bg para usar css
+
+const Navigation = () => {
+
+    return (
+        <div className='containerNav'>
+            <a href="#" className='index1'>Inicio</a>
+            <a href="#" className='index2'>Instrucciones</a>
+       </div>
+    )
+}
+
+export default Navigation
+
