34c5bf2becd2dd4188df2e40034c0eb4ce605763
Alejandro Abad <alejandroabad95@gmail.com>
2023-06-21 20:01:04 +0200

    design first screen finish

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,34 @@ function App() {
   return (
     <>
 
-        <Container className='d-flex flex-column'>
-    
-        <Row>
-          
-          <Col className='d-flex justify-content-center align-items-center'>
-            <a href="#" className='index'>Inicio</a>
-          </Col>
+        <Container>
 
-          <Col className='d-flex justify-content-center align-items-center'>
-            <div className='ball mt-2' style={{ background: '#EF476F'}}>
+        <Row>
+        
+          <Col>
+            <nav className='d-flex justify-content-around'>
+              <a href="#" className='index'>Inicio</a>
+              <a href="#" className='index'>Ayuda</a>
+            </nav>
+          </Col>
+        </Row>
+       
+        <Row> 
+          <Col className=' d-flex justify-content-center'> 
+            <div className='ball' style={{ background: '#EF476F'}}>
               <h2 className='question'>Te encanta hacer</h2>
               <form action="" method="post" className=''>
                 <input type="text" className='custom-form mt-5' />
               </form>
             </div>
           </Col>
-
-          <Col className='d-flex justify-content-center align-items-center'>
-            <a href="#" className='index'>Instrucciones</a>
-          </Col>
         </Row>
 
        
-        <Row style={{height:'27vh'}}>
-          <Col className='d-flex justify-content-center align-content-center'>
+        <Row className='d-flex align-items-center mt-3 mb-3'>
+          
+          <Col className='d-flex justify-content-sm-end justify-content-center'>
+            
             <div className='ball' style={{ background: '#FFD166' }}>
               <h2 className='question'>Eres bueno en</h2>
               <form action="" method="post" className=''>
@@ -40,32 +43,28 @@ function App() {
               </form>
             </div>
           </Col>
-        
 
-          <Col className='d-flex justify-content-center'>
-            <div className='d-flex flex-column justify-content-center mb-5'>
-              <h1 className='mainTitle'>Encuentra tu camino</h1>
-              <div className="text-center">
-                <button className=' mt-3 btnIkigai'>IKIGAI</button>
-              </div>
+          <Col className='d-flex align-items-center justify-content-center text-center'>
+            <div className='m-4 m-md-0'>
+              <h1 className='mainTitle mb-4'>Encuentra tu camino</h1>
+              <button className='btnIkigai'>IKIGAI</button>
             </div>
           </Col>
 
-          <Col className='d-flex justify-content-center'>
-            <div className='ball' style={{ background: '#06D6A0' }}>
+          <Col className='d-flex justify-content-sm-start justify-content-center'>
+
+            <div className='ball ' style={{ background: '#06D6A0' }}>
               <h2 className='question'>Te pueden pagar</h2>
               <form action="" method="post" className=''>
                 <input type="text" className='custom-form mt-5' />
               </form>
             </div>
+
           </Col>
         </Row>
-      
-     
-
        
         <Row>
-          <Col className='d-flex justify-content-center'>
+          <Col className='d-flex justify-content-center mb-5 mb-md-3'>
             <div className='ball' style={{ background: '#26547C' }}>
               <h2 className='question'>El mundo lo necesita</h2>
               <form action="" method="post" className=''>
@@ -74,7 +73,6 @@ function App() {
             </div>
           </Col>
         </Row>
-      
         </Container>
      
     </>
