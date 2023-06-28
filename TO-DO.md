7628d5eb11c127d0c87106622cb3a28456665456
Alejandro Abad <alejandroabad95@gmail.com>
2023-06-28 18:28:58 +0200

    openAI integration

---
+++ TO-DO.md
@@ -0,0 +1,3 @@
+Usa la api de open AI, la idea es que por ejemplo en base a los valores de form1 y form2 generes una respuesta que diga cual podría ser tu pasión en el párrafo que pone primer párrafo, es decir ese texto lo sustituirías por la respuesta en base a esos valores, el resto de párrafos funcionaría de igual manera con los valores de form1, form2, form3, form4,
+
+ChatGPT
--- src/pages/IkigaiFirstPage/IkigaiFirstPage.jsx
+++ src/pages/IkigaiFirstPage/IkigaiFirstPage.jsx
@@ -2,123 +2,200 @@ import { Container, Row, Col } from 'react-bootstrap';
 import "./IkigaiFirstPage.css"
 import Ball from '../../components/Ball/Ball';
 import { useLocation } from 'react-router-dom';
-
+import { useState } from 'react';
+import { useEffect } from 'react';
+import config from '../../config'
 
 import React from 'react';
 
-function IkigaiFirstPage () {
- 
+function IkigaiFirstPage() {
+  
   const handleIkigaiClick = () => {
   }
   const location = useLocation();
   const formValues = location.state?.formValues || {};
 
-return (
-    
-  // Use position: 'relative' for ROWS , top: '50px', mixBlendMode: 'hard-light'
+
+  //useState for openAI
+  const [passion, setPassion] = useState('');
+  const [mission, setMission] = useState('');
+  const [profession, setProfession] = useState('');
+  const [vocation, setVocation] = useState('');
   
-  <Container>
+  useEffect(() => {
+    const fetchOpenAIResponses = async () => {
+      try {
+        // Obtener respuesta para la pasión
+        const passionResponse = await getOpenAIResponse(
+          `¿Cuál podría ser mi pasión si me encanta ${formValues.form1} y soy bueno ${formValues.form2}?`
+        );
+        setPassion(passionResponse);
+        // Obtener respuesta para la misión
+        const missionResponse = await getOpenAIResponse(
+          `¿Cuál podría ser mi misión si me encanta ${formValues.form1} y el mundo necesita ${formValues.form3}?`
+        );
+        setMission(missionResponse);
 
-    <Row className='' style={{height: '200px'}}>
-      
-      <Col><p className='question'>Tu pasión podría ser:</p></Col>
+        // Obtener respuesta para la profesión
+        const professionResponse = await getOpenAIResponse(
+          `¿Cuál podría ser mi profesión si soy bueno ${formValues.form2} y me pueden pagar por ${formValues.form4}?`
+        );
+        setProfession(professionResponse);
 
-      <Col style={{position:'relative', top:'250px', left:'200px'}}><h2 className='secondTitle'>Pasión</h2></Col>
+        // Obtener respuesta para la vocación
+        const vocationResponse = await getOpenAIResponse(
+          `¿Cuál podría ser mi vocación si me puden pagar por ${formValues.form4} y el mundo necesita ${formValues.form3}?`
+        );
+        setVocation(vocationResponse);
+      } catch (error) {
+        console.error('Error calling OpenAI API:', error);
+        // Manejar el error de manera apropiada
+      }
+    };
+
+    fetchOpenAIResponses();
+  }, [formValues]);
+  
+  const getOpenAIResponse = async (prompt) => {
+    const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${config.apiKey}`
+      },
+      body: JSON.stringify({
+        prompt: prompt,
+        max_tokens: 10
+      })
+    });
+
+    const data = await response.json();
+    return data.choices[0].text.trim();
+  }
+    ;
+
+
+  return (
+    
+    // Use position: 'relative' for ROWS , top: '50px', mixBlendMode: 'hard-light'
+  
+    <Container>
+
+      <Row className='' style={{ height: '200px' }}>
+      
+        <Col>
+          <h1 className='promptTitle'>Tu pasión podría ser:</h1>
+          <p className='prompt'>{passion}</p>
+        </Col>
+
+        <Col style={{ position: 'relative', top: '250px', left: '200px' }}>
+          <h2 className='secondTitle'>Pasión</h2>
+        </Col>
       
       
       
-      <Col className=' d-flex justify-content-center'> 
-        <Ball
-        background='#EF476F'
-        width='350px'
-        height='350px'
-        mixBlendMode='hard-light'
-        question= {'Te encanta ' + formValues.form1}
-          showForm={false}
-          mixBlendMode
-        />
-      </Col>
-      <Col style={{ position: 'relative', top: '250px', right: '210px' }}><h2 className='secondTitle'>Misión</h2></Col>
+        <Col className=' d-flex justify-content-center'>
+          <Ball
+            background='#EF476F'
+            width='350px'
+            height='350px'
+            mixBlendMode='hard-light'
+            question={'Te encanta ' + formValues.form1}
+            showForm={false}
+        
+          />
+        </Col>
+
+        <Col style={{ position: 'relative', top: '250px', right: '210px' }}><h2 className='secondTitle'>Misión</h2></Col>
       
-      <Col className=''><p className='question'>Tu misión podría ser:</p></Col>
+        <Col className=''>
+          <h1 className='promptTitle'>Tu misión podría ser:</h1>
+          <p className='prompt'>{mission}</p>
+        </Col>
 
      
       
       
-    </Row>
+      </Row>
 
-    <Row style={{height: '200px'}}>
+      <Row style={{ height: '200px' }}>
 
-      <Col className='d-flex justify-content-end'>
+        <Col className='d-flex justify-content-end'>
 
-        <Ball
-        background='#FFD166'
-        width='350px'
-        height='350px'
-        mixBlendMode='hard-light'
-        question= {'Eres bueno en ' + formValues.form2}
-        showForm={false}
-        />
-      </Col>
+          <Ball
+            background='#FFD166'
+            width='350px'
+            height='350px'
+            mixBlendMode='hard-light'
+            question={'Eres bueno en ' + formValues.form2}
+            showForm={false}
+          />
+        </Col>
 
-      <Col className='d-flex justify-content-start'>
+        <Col className='d-flex justify-content-start'>
       
-        <Ball
-        background='#26547C'
-        width='350px'
-        height='350px'
-        mixBlendMode='hard-light'
-        question= {'El mundo necesita ' + formValues.form3}
-        showForm={false}
-        />
+          <Ball
+            background='#26547C'
+            width='350px'
+            height='350px'
+            mixBlendMode='hard-light'
+            question={'El mundo necesita ' + formValues.form3}
+            showForm={false}
+          />
 
-      </Col>
+        </Col>
 
-    </Row>
+      </Row>
 
-    <Row className=''>
-      <Col><p className='question'>Tu profesión podría ser:</p></Col>
-      <Col style={{position:'relative', left:'200px', top: '50px'}}><h2 className='secondTitle'>Profesión</h2></Col>
+      <Row className=''>
+        <Col>
+          <h1 className='promptTitle'>Tu profesión podría ser:</h1>
+          <p className='prompt'>{profession}</p>
+        </Col>
+        <Col style={{ position: 'relative', left: '200px', top: '50px' }}><h2 className='secondTitle'>Profesión</h2></Col>
 
-      <Col className='d-flex justify-content-center'>
-        <Ball
-        background='#06D6A0'
-        width='350px'
-        height='350px'
-        mixBlendMode='hard-light'
-        question= {'Te pueden pagar por ' + formValues.form4}
-        showForm={false}
-        />
-      </Col>
+        <Col className='d-flex justify-content-center'>
+          <Ball
+            background='#06D6A0'
+            width='350px'
+            height='350px'
+            mixBlendMode='hard-light'
+            question={'Te pueden pagar por ' + formValues.form4}
+            showForm={false}
+          />
+        </Col>
 
-        <Col style={{position:'relative',top:'50px',right:'210px'}}><h2 className='secondTitle'>Vocación</h2></Col>
-        <Col><p className='question'>Tu vocación podría ser:</p></Col>
+        <Col style={{ position: 'relative', top: '50px', right: '210px' }}><h2 className='secondTitle'>Vocación</h2></Col>
+        <Col>
+          <h1 className='promptTitle'>Tu vocación podría ser:</h1>
+          <p className="prompt">{vocation}</p>
+      
+        </Col>
       
         
-    </Row>
+      </Row>
 
-    <Row className=''>
-      <Col className='d-flex justify-content-center'>
-        <button className='btnIkigai' onClick={handleIkigaiClick} style={{ position: 'relative', bottom: '400px' }} >IKIGAI</button>
-      </Col>  
-      <div className='' style={{position:'relative', bottom:'30px'}}>
-        <h1 className='mainTitle d-flex justify-content-center'>Pulsa de nuevo y descubre tu Ikigai</h1>
-      </div>
-    </Row>
+      <Row className=''>
+        <Col className='d-flex justify-content-center'>
+          <button className='btnIkigai' onClick={handleIkigaiClick} style={{ position: 'relative', bottom: '400px' }} >IKIGAI</button>
+        </Col>
+        <div className='' style={{ position: 'relative', bottom: '30px' }}>
+          <h1 className='mainTitle d-flex justify-content-center'>Pulsa de nuevo y descubre tu Ikigai</h1>
+        </div>
+      </Row>
     
-  </Container>
+    </Container>
     
       
     
 
 
 
-)
-
-
-
+  )
 
 }
+
+
 
 
 
